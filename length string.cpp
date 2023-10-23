bb897da8d5fa40410f8dbf4c0bbba2b8af234b7e
ayushisharma1210 <139866680+ayushisharma1210@users.noreply.github.com>
2023-10-23 15:03:41 +0530

    Add files via upload

---
+++ length string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,len;
+    char name[3456];
+    cout<< "enter name";
+    cin>> name;
+    cout<<"you entered"<< name <<endl;
+    while(name [i != '\0'])
+{
+    len++;
+    i++;
+}
+cout<<"length of name is"<<len;
+}
